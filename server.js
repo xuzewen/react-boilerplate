7e6e0259c986e41e42b0b57a574cb18d71adea47
xuzewen <xzw_78154@sina.com>
2016-07-15 15:46:34 +0800

    “简化reudx”

--- server.js
+++ server.js
@@ -1,14 +1,7 @@
-var path = require('path');
-var fs = require('fs');
-
-
-// no code assistance in nodejs
-var express = require('express');
 var WebpackDevServer = require('webpack-dev-server')
 var webpack = require('webpack');
 var config = require('./webpack.config');
-var app = express();
-var http = require('http');
+
 var port = '3000';
 var hosts = '0.0.0.0'
 
@@ -21,40 +14,27 @@ for (var key in config['entry']) {
 
 var compiler = webpack(config);
 
+var compiler = webpack(config);
 
-if (process.argv[2] == 'build') {
-    app.use('/', express.static(__dirname + '/build'));
-    /* 创建UI静态服务器 */
-    http.createServer(app).listen(3000, '0.0.0.0', function (err) {
-        if (err) {
-            console.log(err);
-            return false;
-        }
-    });
-    console.log(__dirname + '/build');
-} else if (process.argv[2] == 'dev') {
-    var compiler = webpack(config);
+var server = new WebpackDevServer(compiler, {
+    hot:true,
+    //热加载必须的 inline
+    inline:true,
 
-    var server = new WebpackDevServer(compiler, {
-        hot:true,
-        //热加载必须的 inline
-        inline:true,
+    quiet: false,
+    compress: false,
+    historyApiFallback: true,
+    stats: {
+        // Config for minimal console.log mess.
+        assets: true,
+        colors: true,
+        version: false,
+        hash: true,
+        timings: true,
+        chunks: false,
+        chunkModules: true
+    }
+});
 
-        quiet: false,
-        compress: false,
-        historyApiFallback: true,
-        stats: {
-            // Config for minimal console.log mess.
-            assets: true,
-            colors: true,
-            version: false,
-            hash: true,
-            timings: true,
-            chunks: false,
-            chunkModules: true
-        }
-    });
-
-    server.listen(port);
-    console.log(`Listening at http://${hosts}:${port}`);
-}
+server.listen(port);
+console.log(`Listening at http://${hosts}:${port}`);
