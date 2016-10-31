dd273a9e78afc8b9ff36a7b99b7064338c7e2baf
xuzewen <xzw_78154@sina.com>
2016-10-31 15:42:26 +0800

    “测试”

--- src/index.js
+++ src/index.js
@@ -5,11 +5,37 @@ import { App } from './components/App';
 import { List } from './components/list';
 
 class Meun extends Component {
+    constructor(props){
+        super(props)
+        this.state = {}
+        this.state.c = function(){
+            alert('c')
+        }
+    }
+
+    componentDidMount(){
+        this.setState({
+            c:function(){
+                alert('c2')
+            }
+        })
+    }
+
+    a(){
+        alert('a')
+    }
+
+    b(){
+        alert('b')
+
+        console.log(this.refs)
+    }
+
     render () {
         return (
             <div>
-                <h1 className="middle">这是index页面</h1>
-                <h2 className="blue">这是入口index加载的list</h2>
+                <h1 className="middle" onClick={this.a} onClick={this.b.bind(this)} onClick={this.state.c.bind(this)}ref='a'>这是index页面</h1>
+                <h2 className="blue" ref='a'>这是入口index加载的list</h2>
                 <List />
             </div> 
         );
