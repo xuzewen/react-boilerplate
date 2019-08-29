347474eed224d4c77c09e9c16d2b056f1bf8bc86
徐泽文 <xuzw@weipaitang.com>
2019-08-29 09:51:34 +0800

    sh

---
+++ a.sh
@@ -0,0 +1,1 @@
+/script T,F=T or 0,F or CreateFrame("frame")if X then X=nil print("OFF.")else print("ON.") X=function()local t=GetTime()if t-T>1 then StaticPopup1Button2:Click()T=t end end end F:SetScript("OnUpdate",X)
